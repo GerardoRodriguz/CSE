62971c09165899db46e26a826450fa7d5663450c
GerardoRodriguz <31050428+GerardoRodriguz@users.noreply.github.com>
2017-11-28 15:07:21 -0800

    Adding Previous Notes
    11.27.17

---
+++ Notes2.py
@@ -0,0 +1,31 @@
+print("Hello World")
+
+
+
+
+
+
+
+print(3**2)
+
+print()#Creates a blank line
+print("See if you can figure this out")
+print(15%5)
+
+# Varibles
+car_name = "Wiebe Mobile"
+car_type = "Lamborghini Sesto Elemento"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline Printing
+print("My car is the ")
+
+
+# Taking input
+name = input("What is your name?")
+print("Hello %s." % name)
+# print(name)
+# 
+age = input("How old are you")
+print("%s? You are young")
