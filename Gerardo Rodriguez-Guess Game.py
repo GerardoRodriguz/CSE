812f3aa0f5ef00344a692fe74e774378019b5d4f
GerardoRodriguz <31050428+GerardoRodriguz@users.noreply.github.com>
2017-12-06 15:39:28 -0800

    12/6/17
    Guess Game

--- Gerardo Rodriguez-Guess Game.py
+++ Gerardo Rodriguez-Guess Game.py
@@ -3,7 +3,8 @@ import random
 print(random.randint(0, 50))
 
 
-def
+def random_randint(input):
+    print()
 
 
 
@@ -33,10 +34,13 @@ def
 
 
 
-# 1.Generate a random number
-# 2.Get a number (input) from the user
-# 3.Compare number to input
-# 4.Add "Higher" or "Lower"
-# 5.Add 5 guesses
-# "1" == 1 false
-# Varible for guesses
+
+
+
+ #  2.Get a number (input) from the user
+ #  3.Compare number to input
+ #  4.Add "Higher" or "Lower"
+ #  5.Add 5 guesses
+ #  1.Generate a random number
+ #  "1" == 1 false
+ #  Varible for guesses
